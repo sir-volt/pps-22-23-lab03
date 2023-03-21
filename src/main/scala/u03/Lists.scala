1e01a17cff0b022bc10bf8dc5f1c60e661aa3ec2
Andrea Bianchi <andrea.bianchi24@studio.unibo.it>
2023-03-21 22:38:03 +0100

    fixed build.properties configuration, modified ListTest adding test for Drop, Append and FlatMap methods, created and completed previous said methods (except for FlatMap)

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -21,6 +21,18 @@ object Lists extends App :
       case Cons(h, t) if pred(h) => Cons(h, filter(t)(pred))
       case Cons(_, t) => filter(t)(pred)
       case Nil() => Nil()
+    def drop[A](l1: List[A])(dropper: Int): List[A] = l1 match
+      case Cons(_, t) if dropper > 1 => drop(t)(dropper - 1)
+      case Cons(_, t) => t
+      case _ => Nil()
+
+    def append[A](left: List[A])(right: List[A]): List[A] = left match
+      case Cons(h, t) => Cons(h, append(t)(right))
+      case Nil() => right
+
+    def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match
+      //case Cons(h, t) => Cons(f(h), flatMap(t)(f))
+      case Nil() => Nil()
 
   val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
   println(List.sum(l)) // 60
--- src/test/scala/u03/ListTest.scala
+++ src/test/scala/u03/ListTest.scala
@@ -20,3 +20,15 @@ class ListTest:
   @Test def testFilter() =
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_>=20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_!=20))
+
+  @Test def testDrop() =
+    assertEquals(Cons(20, Cons(30, Nil())), drop(l)(1))
+    assertEquals(Cons(30, Nil()), drop(l)(2))
+    assertEquals(Nil(), drop(l)(5))
+
+  @Test def testAppend() =
+    assertEquals(Cons(40,Cons(10, Cons(20, Cons(30, Nil())))), append(Cons(40, Nil()))(l))
+    assertEquals(Cons(40, Cons(50, Cons(10, Cons(20, Cons(30, Nil()))))), append(Cons(40, Cons(50, Nil())))(l))
+
+  @Test def testFlatMap() =
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
